5c087d098eff5b20c73bf5cad05b49f0eca7310d
Oscar Boykin <oscar@stripe.com>
2018-08-29 14:46:03 -1000

    fix build

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,10 +1,13 @@
 import sbt._
 
 object Dependencies {
+
+  lazy val catsVersion = "1.2.0"
+
   lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.13.4"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
-  lazy val cats = "org.typelevel" %% "cats-core" % "1.2.0"
-  lazy val catsLaws = "org.typelevel" %% "cats-laws" % "1.2.0"
-  lazy val catsTestKit = "org.typelevel" %% "cats-testkit" % "1.1.0"
+  lazy val cats = "org.typelevel" %% "cats-core" % catsVersion
+  lazy val catsLaws = "org.typelevel" %% "cats-laws" % catsVersion
+  lazy val catsTestKit = "org.typelevel" %% "cats-testkit" % catsVersion
   lazy val fastparse = "com.lihaoyi" %% "fastparse" % "1.0.0"
 }
